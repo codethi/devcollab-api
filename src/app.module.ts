11080751a9dc217e0363ce2f1fbffd2795f830a0
codethi <docencia.thiago@gmail.com>
2023-06-13 20:13:04 -0300

    feat: create auth resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { QuestionsModule } from './modules/questions/questions.module';
 import { AnswersModule } from './modules/answers/answers.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [UsersModule, QuestionsModule, AnswersModule],
+  imports: [UsersModule, QuestionsModule, AnswersModule, AuthModule],
   controllers: [],
   providers: [],
 })
